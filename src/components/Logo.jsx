6e44fef6d5659ac99dc58927e4e3818d823d8595
rufataliy <rufataliyevbakou@gmail.com>
2020-01-18 03:57:27 -0500

    added loader

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import logoImg from "../logo.png"
+const Logo = () => {
+
+    return (
+        <img className="loading" src={logoImg} alt="" />
+    )
+}
+
+export default Logo
