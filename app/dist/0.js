89c908bafa0fa0c7d74e7719e1b80b3bd9660ce0
Rufat Aliyev <rufataliy@users.noreply.github.com>
2020-06-02 23:37:45 -0400

    added dist

---
+++ app/dist/0.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/public/img/loading.gif":
+/*!************************************!*\
+  !*** ./src/public/img/loading.gif ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "1b0e3a2f4c3e8131ca07ecb7d615ab3d.gif";
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.js.map
