f2caebd32e8fb69042230d0b3ae7f47036faede1
Rufat Aliyev <rufataliy@users.noreply.github.com>
2020-06-03 21:56:32 -0400

    fixing file upload path

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -3,10 +3,10 @@ module.exports = (req, res, next) => {
   if (req.files) {
     req.body.fileName = req.files.upload.name;
     req.body.filePath = path.join(
-      process.env.BASE_URL + "/css/img/" + req.files.upload.name
+      process.env.BASE_URL + "img/" + req.files.upload.name
     );
     req.files.upload
-      .mv(path.join(__dirname, "../public/css/img/" + req.files.upload.name))
+      .mv(path.join(__dirname, "../public/img/" + req.files.upload.name))
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }
--- routes/fileuploads.js
+++ routes/fileuploads.js
@@ -3,8 +3,6 @@ const router = new express.Router();
 const fileUpload = require("../middlewares/fileUpload");
 
 router.post("/textEditorImageUpload", fileUpload, (req, res) => {
-  console.log("works");
-
   res
     .status(200)
     .send({ url: process.env.BASE_URL + "img/" + req.files.upload.name });
