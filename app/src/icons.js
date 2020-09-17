4cc3bb7b7ef1a328129914064c62602e2b373d9a
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-16 22:54:04 -0400

    Importing core ui icons

---
+++ app/src/icons.js
@@ -0,0 +1,11 @@
+export {
+  cilHome as home,
+  cilCalendar as schedule,
+  cilLibrary as programs,
+  cilTranslate as vocabularies,
+  cibReadme as lesson,
+  cilAccountLogout as logout,
+  cilCalendar as date,
+  cilClock as time,
+  cilDescription as section,
+} from "@coreui/icons";
