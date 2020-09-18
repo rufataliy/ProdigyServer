54d8e9759a9e56cbe96c603b440923ecf4fc9411
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-17 22:19:03 -0400

    Removed unused files

--- app/src/components/Modal/ModalChildren.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FormikForm } from "../Form/form.jsx";
-import { DeleteConfirm } from "./DeleteConfirm.jsx";
-
-export const children = {
-  FormimForm: <FormikForm />,
-  DeleteConfirm: "<DeleteConfirm />",
-};
