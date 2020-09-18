a8d500585dc78faccfd02cc70310bb3aa2807ec1
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-17 21:54:41 -0400

    Converted to coreui

--- app/src/components/Word/Word.jsx
+++ app/src/components/Word/Word.jsx
@@ -1,49 +1,54 @@
-import React from "react";
-import { Accordion, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  CCard,
+  CCollapse,
+  CCardBody,
+  CCardHeader,
+  CCardTitle,
+  CCardFooter,
+} from "@coreui/react";
 import Icon from "../../views/_Icon.jsx";
 
 const Word = ({ item, editItem, deleteItem, readOnly }) => {
+  const [collapse, setCollapse] = useState(false);
+
   return (
-    <Accordion className="m-2">
-      <Card style={{ width: "18rem" }}>
-        <Accordion.Toggle
-          style={{ cursor: "pointer" }}
-          as={Card}
-          variant="link"
-          eventKey={item._id}
-        >
-          <Card.Header className="d-flex align-items-center justify-content-between">
-            {`${item.phrase}`}
-            {!readOnly && (
-              <div>
-                <Icon
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    editItem(item);
-                  }}
-                  className="ml-3 btn-crud fas fa-pen"
-                />
-                <Icon
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    deleteItem(item);
-                  }}
-                  className="ml-3 btn-crud fas fa-trash"
-                />
-              </div>
-            )}
-          </Card.Header>
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey={item._id}>
-          <Card.Body>
-            <p className="text-secondary">Definition</p>
-            <p>{item.definition}</p>
-            <p className="text-secondary">Example</p>
-            <p>{item.example}</p>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+    <CCard className="word mr-3 mb-3">
+      <CCardHeader
+        className="cursor-pointer"
+        onClick={() => setCollapse(!collapse)}
+      >
+        <CCardTitle>{`${item.phrase}`}</CCardTitle>
+      </CCardHeader>
+      <CCollapse show={collapse}>
+        <CCardBody>
+          <p>Definition</p>
+          <p className="word-text-secondary">{item.definition}</p>
+          <p>Example</p>
+          <p className="word-text-secondary">{item.example}</p>
+        </CCardBody>
+      </CCollapse>
+      <CCardFooter>
+        {!readOnly && (
+          <div>
+            <Icon
+              onClick={(e) => {
+                e.stopPropagation();
+                editItem(item);
+              }}
+              className="ml-3 btn-crud fas fa-pen"
+            />
+            <Icon
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteItem(item);
+              }}
+              className="ml-3 btn-crud fas fa-trash"
+            />
+          </div>
+        )}
+      </CCardFooter>
+    </CCard>
   );
 };
 export default React.memo(Word);
