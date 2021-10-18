683c21ac8c9a01cd9174a96aa8779cefb6e8ba2a
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-10-17 19:32:29 -0700

    Added sample flag to sample user

--- tools/defaults.js
+++ tools/defaults.js
@@ -1,5 +1,6 @@
 module.exports.sampleUser = {
     name: "Sample User",
     email: "sample_user@example.com",
-    avatar:"https://robohash.org/cb50f92085a897a90edf757cad009a7d?set=set4&bgset=&size=200x200"
+    avatar:"https://robohash.org/cb50f92085a897a90edf757cad009a7d?set=set4&bgset=&size=200x200",
+    sample: true
 }
