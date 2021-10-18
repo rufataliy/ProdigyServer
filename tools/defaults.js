4d1104fa79726d7f37c05abc1dab927ae4ce052b
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-10-17 19:27:38 -0700

    Added default user for demo

---
+++ tools/defaults.js
@@ -0,0 +1,5 @@
+module.exports.sampleUser = {
+    name: "Sample User",
+    email: "sample_user@example.com",
+    avatar:"https://robohash.org/cb50f92085a897a90edf757cad009a7d?set=set4&bgset=&size=200x200"
+}
