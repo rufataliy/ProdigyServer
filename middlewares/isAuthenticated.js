e7e5624f70b41f77e88ee0d458d1658595ca13cc
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-10-17 19:29:14 -0700

    Added logic to create sample user if it doesn't exist

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,33 +1,43 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { sampleUser } = require('../tools/defaults');
 
 const persistUserToCookie = (user, res) => {
   jwt.sign(user, process.env.JWT_SECRET, (err, token) => {
     if (!err) {
-      res.cookie("user", token);
+      res.cookie('user', token);
     }
   });
 };
 
 const isAuthenticated = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    return User.findOne({ sample: true }, {}, { lean: true }, (err, user) => {
+    User.findOne({ sample: true }, {}, { lean: true }, (err, user) => {
       if (!err) {
         req.user = user;
         persistUserToCookie(req.user, res);
+        next();
+      } else {
+        User.create(sampleUser).then((user) => {
+          req.user = user;
+          next();
+        });
       }
-      next();
     });
-  }
+  } else {
+    const { email } = req.openid.user;
 
-  const { email } = req.openid.user;
-
-  User.findOne({ email }, {}, { lean: true }, (err, user) => {
-    if (!err) {
-      persistUserToCookie(req.user, res);
-    }
-    next();
-  });
+    User.findOne({ email }, {}, { lean: true }, (err, user) => {
+      if (!err) {
+        req.user = user;
+        persistUserToCookie(user, res);
+        next();
+      }else {
+        res.redirect("/login");
+      }
+      
+    })
+  } 
 };
 
 module.exports = isAuthenticated;
